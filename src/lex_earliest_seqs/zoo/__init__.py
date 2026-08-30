d70761a7d1cb15bd8b023a9c57b61367707b5ed9
natelastname <44933154+natelastname@users.noreply.github.com>
2026-08-30 16:33:11 -0400

    Export finite-Omega EW family helpers

--- src/lex_earliest_seqs/zoo/__init__.py
+++ src/lex_earliest_seqs/zoo/__init__.py
@@ -5,12 +5,19 @@ from __future__ import annotations
 from ..core import SequenceRegistry
 from .binary_enots_wolley import BINARY_ENOTS_WOLLEY, BinaryEnotsWolleyGenerator
 from .enots_wolley import ENOTS_WOLLEY, EnotsWolleyGenerator
+from .factor_restricted_enots_wolley import (
+    EWFactorPolicy,
+    FactorRestrictedEnotsWolleyGenerator,
+    big_omega,
+    make_factor_restricted_enots_wolley_definition,
+)
 from .forced_squarefree_enots_wolley import (
     FORCED_SQUAREFREE_ENOTS_WOLLEY,
     ForcedSquarefreeEnotsWolleyGenerator,
 )
 from .squarefree_semiprime_enots_wolley import (
     SQUAREFREE_SEMIPRIME_ENOTS_WOLLEY,
+    X000000_POLICY,
     SquarefreeSemiprimeEnotsWolleyGenerator,
 )
 
@@ -18,11 +25,16 @@ __all__ = [
     "BINARY_ENOTS_WOLLEY",
     "BinaryEnotsWolleyGenerator",
     "ENOTS_WOLLEY",
+    "EWFactorPolicy",
     "EnotsWolleyGenerator",
     "FORCED_SQUAREFREE_ENOTS_WOLLEY",
+    "FactorRestrictedEnotsWolleyGenerator",
     "ForcedSquarefreeEnotsWolleyGenerator",
     "SQUAREFREE_SEMIPRIME_ENOTS_WOLLEY",
     "SquarefreeSemiprimeEnotsWolleyGenerator",
+    "X000000_POLICY",
+    "big_omega",
+    "make_factor_restricted_enots_wolley_definition",
     "register_builtins",
 ]
 
